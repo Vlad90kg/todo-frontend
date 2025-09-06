817a28a64ff7421fc127392f61e8824a702e3dfe
vladikslon <vladbrovi@gmail.com>
2025-09-07 00:23:04 +0100

    add api functions

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import 'ui/checkbox';
 import 'ui/button';
 import {Todo} from '@/types';
 import { useState } from "react";
+import * as content from '@/services/api';
 
 export default function TodoList () {
     const [todos, setTodos] = useState<Todo[]>([]);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,30 @@
-export {};
+import axios from 'axios';
+
+import {Todo} from 'types';
+
+const api = axios.create({baseURL:'http://localhost:8080/api'});
+
+export async function getTodos(): Promise<Todo[]> {
+    const res = await api.get<Todo[]>('todo');
+    return res.data;
+}
+
+
+export async function getTodo(id:string): Promise<Todo> {
+    const res = await  api.get<Todo>('todo/${id}');
+    return res.data;
+}
+
+export async function deleteTodo(id:string): Promise<void> {
+    await  api.delete('todo/${id}');
+}
+
+export async function addTodo(todo: Omit<Todo, "id">) {
+    const res = await api.post('todo');
+    return res.data;
+}
+
+export async function updateTodo(todo:Todo) {
+    const res = await api.put<Todo>(`todo/${todo.id}`, todo);
+    return res.data;
+}
